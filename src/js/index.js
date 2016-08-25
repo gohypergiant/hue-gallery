1c8364819b70456c4cc3389c237ea2756435ba0c
Brad Cerasani <bradcerasani@gmail.com>
2016-08-25 11:23:27 -0500

    Add per-image swatches, remove lazyloading support

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,52 @@
 /* global ColorThief */
 import md5 from 'md5';
 import Blazy from 'blazy';
-import debounce from 'lodash/debounce';
+import throttle from 'lodash/throttle';
 import forEach from 'lodash/forEach';
-import getImages from './crawl';
+import filter from 'lodash/filter';
+import isVisible from './visible';
+
+const allImages = document.getElementsByTagName('img');
 
 const blazy = new Blazy(); // eslint-disable-line
 const thief = new ColorThief();
-const debounceSpeed = 250;
+const throttleSpeed = 200;
 const colorCache = {};
 
-const colorBoxes = document.querySelectorAll('.Colorbox-swatch');
+function printSwatches(colors, img) {
+  const elSwatchBox = document.createElement('div');
+  elSwatchBox.classList.add('SwatchBox');
+  img.parentNode.appendChild(elSwatchBox);
 
-function isLoaded(el) {
-  return el.complete && el.naturalHeight !== 0;
+  forEach(colors, color => {
+    const elSwatch = document.createElement('div');
+    elSwatch.classList.add('Swatch');
+    elSwatch.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]}`;
+    elSwatchBox.appendChild(elSwatch);
+  });
 }
 
 function getSwatches(key, img) {
-  if (!colorCache[key]) {
-    const colors = thief.getPalette(img, 2);
-    colorCache[key] = colors;
-  }
+  const colors = thief.getPalette(img, 11);
+  colorCache[key] = colors;
 
+  printSwatches(colors, img);
   return colorCache[key];
 }
 
-function setColorboxes(swatches) {
-  forEach(swatches, (swatch, i) => {
-    colorBoxes[i].style.backgroundColor = `rgb(${swatch[0]}, ${swatch[1]}, ${swatch[2]})`;
-  });
+forEach(allImages, img => {
+  const key = md5(img.getAttribute('src'));
+
+  img.addEventListener('load', e => {
+    getSwatches(key, e.target);
+  }, false);
+});
+
+function getImage() {
+  console.log(filter(allImages, isVisible));
 }
 
-function createColors() {
-  const images = getImages();
-
-  // We do not want to trigger a recolor if two images are visable at a time
-  if (images.length > 1) {
-    return;
-  }
-
-  forEach(images, img => {
-    let swatches = null;
-    let key = null;
-
-    if (isLoaded(img)) {
-      key = md5(img.getAttribute('src'));
-      swatches = getSwatches(key, img);
-      setColorboxes(swatches);
-    } else {
-      img.addEventListener('load', e => {
-        const el = e.target;
-        key = md5(el.getAttribute('src'));
-        swatches = getSwatches(key, el);
-        setColorboxes(swatches);
-      }, false);
-    }
-  });
-}
-
-createColors();
-window.addEventListener('scroll', debounce(createColors, debounceSpeed), false);
+window.addEventListener('scroll', throttle(getImage, throttleSpeed), false);
 
 if (module.hot) {
   module.hot.accept();
--- src/js/visible.js
+++ src/js/visible.js
@@ -1,12 +1,8 @@
 export default function isVisible(el) {
-  const { right, bottom, top, left } = el.getBoundingClientRect();
-  const vWidth = window.innerWidth;
-  const vHeight = window.innerHeight;
+  const { bottom, top } = el.getBoundingClientRect();
 
   return (
-    right > 0 &&
     bottom > 0 &&
-    left < vWidth &&
-    top < vHeight
+    top < window.innerHeight
   );
 }
